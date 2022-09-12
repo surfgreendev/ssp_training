61c202fc04bd53bd0f9604ea5e7a430fae9ac511
Carl Bednorz <carl@surfgreen.dev>
2022-09-12 08:32:26 +0200

    my update

--- self_service_portal/blog/views.py
+++ self_service_portal/blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
+from django.views import View
 from self_service_portal.blog.models import Post
 
 # Create your views here.
@@ -13,3 +14,11 @@ class PostCreateView(CreateView):
     model = Post
     fields = ["title", "sub_title", "status", "post_image"]
     template_name = "blog/create_post.html"
+
+
+"""
+class PostListView2(View):
+    def get(self, request, *args, **kwargs):
+        posts = Post.objects.published.all()
+        return render(request, "blog.list_post_2.html", {"posts": posts})
+"""
